bd68a5da3e3996ec6cb1dafba6cd6f1b37baa729
dnavnu534 <21020534@vnu.edu.vn>
2023-12-17 13:32:08 +0700

    fix tracking page UI

--- src/app/component/admin/manager-storage/home/manager-storage.component.ts
+++ src/app/component/admin/manager-storage/home/manager-storage.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./manager-storage.component.css']
 })
 export class ManagerStorageComponent {
-
+  
 }
--- src/app/service/manager/manager.service.ts
+++ src/app/service/manager/manager.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManagerService {
+  private baseUrl = "http://localhost:8080/api/v1/manager";
+  username: string | null = '';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private authService: AuthService) { }
+
+  getUserName() {
+    if (sessionStorage.getItem('role') == "ROLE_MANAGER_STORAGE") {
+      this.username = sessionStorage.getItem('username');
+    }
+    return this.username;
+  }
+
+  
 }
