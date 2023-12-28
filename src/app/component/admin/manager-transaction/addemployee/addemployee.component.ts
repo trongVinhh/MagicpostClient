59ec991ca1bd96a00bcd53c38eb991faea58fd34
Nguyễn Đức Anh <datnguyen1854@gmail.com>
2023-12-28 12:15:38 +0700

    tao moi nhan vien

---
+++ src/app/component/admin/manager-transaction/addemployee/addemployee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addemployee',
+  standalone: true,
+  imports: [],
+  templateUrl: './addemployee.component.html',
+  styleUrl: './addemployee.component.css'
+})
+export class AddemployeeComponent {
+
+}
